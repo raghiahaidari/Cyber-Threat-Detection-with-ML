44277c7a7dc16b771a5f3d80d88445ea0f39c271
ElhoubeBrahim <brahim.elhoube@gmail.com>
2024-03-09 00:11:12 +0100

    header UI implementation

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+	<header>
+    <div class="container py-6">
+      <div class="flex items-center justify-center">
+        <img src="/logo.svg" alt="Secure Web" class="h-[100px] select-none" />
+      </div>
+    </div>
+  </header>
+</template>
